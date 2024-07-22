fa538419474d31c08affa224229d2c049600f2e0
Ahmet Caliskan <root@systemalert.org>
2024-07-22 22:36:08 +0200

    change deprecated lsp plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -79,6 +79,7 @@ return packer.startup(function(use)
   use { "williamboman/mason-lspconfig.nvim", commit = "0051870dd728f4988110a1b2d47f4a4510213e31" }
 	use { "jose-elias-alvarez/null-ls.nvim", commit = "c0c19f32b614b3921e17886c541c13a72748d450" } -- for formatters and linters
   use { "RRethy/vim-illuminate", commit = "d6ca7f77eeaf61b3e6ce9f0e5a978d606df44298" }
+  use {"nvimtools/none-ls.nvim"}
 
 	-- Telescope
 	use { "nvim-telescope/telescope.nvim", commit = "76ea9a898d3307244dce3573392dcf2cc38f340f" }
